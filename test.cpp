2498501ac90ed41b25e81e39d9b39474bd41a255
hugo valle <hugovalle1@weber.edu>
2018-03-15 12:57:21 -0600

    Added two more test: Super range Negative range

--- test.cpp
+++ test.cpp
@@ -20,3 +20,38 @@ TEST_CASE("1) Test Constructor")
         REQUIRE(arrayToString(sa) == "20 21 22 23 24");
     }
 }
+TEST_CASE("2) Test Constructor with super range")
+{
+    INFO("Testing SuperArray class with super indexes");
+    int low = 5; // low index: 5, 6, 7, 8, 9
+    int cap = 5;
+    int high = low + cap -1; // high index
+    // Create a instance of the SuperArray
+    SuperArray sa(low, cap);
+    for(int i = low; i <= high; i++)
+    {
+        sa[i] = i + 100;
+    }
+    SECTION("Test #2: Super Index Constructor")
+    {
+        REQUIRE(arrayToString(sa) == "105 106 107 108 109");
+    }
+}
+
+TEST_CASE("3) Test Constructor with negative index")
+{
+    INFO("Testing SuperArray class with negative indexes");
+    int low = -20; // low index: -20, -19, -18, 17
+    int cap = 5;
+    int high = low + cap -1; // high index
+    // Create a instance of the SuperArray
+    SuperArray sa(low, cap);
+    for(int i = low; i <= high; i++)
+    {
+        sa[i] = i - 10;
+    }
+    SECTION("Test #3: Constructor")
+    {
+        REQUIRE(arrayToString(sa) == "-30 -29 -28 -27 -26");
+    }
+}
